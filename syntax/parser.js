de5e40c383f44261c198b68082fe760cf1faaa42
John Lopez <johnllopez616@gmail.com>
2019-02-08 00:40:45 -0800

    Added require statements

--- syntax/parser.js
+++ syntax/parser.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const ohm = require('ohm-js');
+const withIndentsAndDedents = require('./preparser');
+
+const grammar = ohm.grammar(fs.readFileSync('./syntax/stonescript. ohm'));
+
 module.exports = (text) => {
   const match = grammar.match(withIndentsAndDedents(text));
   if (!match.succeeded()) {
