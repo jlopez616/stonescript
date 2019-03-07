47d5205d3d091ab0e3c03962b4ecdd958811dc24
aboyac <aboyac@lion.lmu.edu>
2019-03-07 15:11:36 -0800

    Restored Toal's help that you removed...

--- syntax/syntax-checker.js
+++ syntax/syntax-checker.js
@@ -1,8 +1,28 @@
 const fs = require('fs');
 const ohm = require('ohm-js');
+const Program = require('../ast/program.js');
+const VariableDeclaration = require('../ast/variable-declaration.js');
+const Assignment = require('../ast/assignment.js');
+const Squish = require('../ast/squish-assignment.js');
+const Rip = require('../ast/rip-assignment.js');
 
-const grammar = ohm.grammar(fs.readFileSync('syntax/stonescript.ohm'));
+const grammar = ohm.grammar(fs.readFileSync('./syntax/stonescript.ohm', 'utf-8'));
+
+/* eslint-disable no-unused-vars */
+const astGenerator = grammar.createSemantics().addOperation('ast', {
+  Program(statements) { return new Program(statements.ast()); },
+  Declaration(_1, id, _2, exp) { return new VariableDeclaration(id.ast(), exp.ast()); },
+  NormalAssignment(id, _1, exp) { return new Assignment(id.ast(), exp.ast()); },
+  LittleAssignment_squish(id, _1, _2) { return new Squish(id.ast()); },
+  LittleAssignment_rip(id, _1, _2) { return new Rip(id.ast()); },
+  /* Call(functionName, _1, params, _2) { return new Call(functionName.ast(), params.ast())} */
+});
+/* eslint-enable no-unused-vars */
 
 module.exports = (text) => {
-  return grammar.match(text).succeeded();
+  const match = grammar.match(text);
+  if (!match.succeeded()) {
+    return false;
+  }
+  return astGenerator(match).ast();
 };
