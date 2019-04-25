0efaac434f2075ff43c5f7ac422292513b7cb9cf
aboyac <aboyac@lion.lmu.edu>
2019-04-25 16:44:59 -0700

    Added skeleton

--- semantics/analyzer.js
+++ semantics/analyzer.js
@@ -9,9 +9,138 @@ const { CounterType, WorderType, YesnosType, WhatType, TabletType } = require('.
 
 const check = require('./check');
 
+Argument.prototype.analyze = (context) => {
+
+};
+
 Array.prototype.analyze = (context) => {
-  constructor(args) {
     this.args = context.lookupType(this.args); //tells us the type of the array
-  }
-  
-}
+};
+
+Assignment.prototype.analyze = (context) => {
+// TODO
+};
+
+Exp1_binary.prototype.analyze = (context) => {
+  // TODO
+};
+
+Exp2_binary.prototype.analyze = (context) => {
+  // TODO
+};
+
+Exp3_binary.prototype.analyze = (context) => {
+  // TODO
+};
+
+BooleanLiteral.prototype.analyze = (context) => {
+  // TODO
+};
+
+Conditional.prototype.analyze = (context) => {
+  // TODO
+};
+
+BooleanLiteral.prototype.analyze = (context) => {
+  // TODO
+};
+
+Call.prototype.analyze = (context) => {
+  // TODO
+};
+
+Declaration.prototype.analyze = (context) => {
+  // TODO
+};
+
+ForLoop.prototype.analyze = (context) => {
+  // TODO
+};
+
+FunctionDeclaration.prototype.analyze = (context) => {
+  // TODO
+};
+
+FunctionObject.prototype.analyze = (context) => {
+  // TODO
+};
+
+IfStatement.prototype.analyze = (context) => {
+  // TODO
+};
+
+NumericLiteral.prototype.analyze = (context) => {
+  // TODO
+};
+
+Parameter.prototype.analyze = (context) => {
+  // TODO
+};
+
+Postfix.prototype.analyze = (context) => {
+  // TODO
+};
+
+Program.prototype.analyze = (context) => {
+  // TODO
+};
+
+RelExp.prototype.analyze = (context) => {
+  // TODO
+};
+
+ReturnStatement.prototype.analyze = (context) => {
+  // TODO
+};
+
+RipAssignment.prototype.analyze = (context) => {
+  // TODO
+};
+
+Func.prototype.analyze = (context) => {
+  // TODO
+};
+
+Return.prototype.analyze = (context) => {
+  // TODO
+};
+
+SquishAssignment.prototype.analyze = (context) => {
+  // TODO
+};
+
+Statement.prototype.analyze = (context) => {
+  // TODO
+};
+
+StringLiteral.prototype.analyze = (context) => {
+  // TODO
+};
+
+UnaryExpression.prototype.analyze = (context) => {
+  // TODO
+};
+
+VariableDeclaration.prototype.analyze = (context) => {
+  // TODO
+};
+
+WhileLoop.prototype.analyze = (context) => {
+  // TODO
+};
+
+Literal.prototype.analyze = (context) => {
+  // TODO
+};
+
+intlit.prototype.analyze = (context) => {
+  // TODO
+};
+
+Obj.prototype.analyze = (context) => {
+  // TODO
+};
+
+Exp_or.prototype.analyze = (context) => {
+  // TODO
+};
