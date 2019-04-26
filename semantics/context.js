24bd625d6a10fb65cf3d2099d2ec4f9018fba351
aboyac <aboyac@lion.lmu.edu>
2019-04-25 17:16:30 -0700

    Changed the file from plainscript's context to tiger's

--- semantics/context.js
+++ semantics/context.js
@@ -1,84 +1,111 @@
-/* 
- * From Ray Toal's plainscript
+/* Taken from Tiger
  *
  * Semantic Analysis Context
  *
- * A context object holds state for the semantic analysis phase, such as the
- * enclosing function (if any), whether or not we are in a loop, a map of
- * declarations introduced in this scope, and the parent context.
+ * A context object holds state for the semantic analysis phase.
  *
  *   const Context = require('./semantics/context');
  */
 
-const FunctionDeclaration = require('../ast/function-declaration');
-const FunctionObject = require('../ast/function-object');
-const Parameter = require('../ast/parameter');
+const { standardFunctions, IntType, StringType, NilType } = require('./builtins');
+
+require('./analyzer');
+
+// When doing semantic analysis we pass around context objects.
+//
+// A context object holds:
+//
+//   1. A reference to the parent context (or null if this is the root context).
+//      This allows to search for declarations from the current context outward.
+//
+//   2. A reference to the current function we are analyzing, if any. If we are
+//      inside a function, then return expressions are legal, and we will be
+//      able to type check them.
+//
+//   3. Whether we are in a loop (to know that a `break` is okay).
+//
+//   4. A map for looking up types declared in this context.
+//
+//   5. A map for looking up vars and functions declared in this context.
+//
+// The reason for the two maps is that in Tiger, types are kept in a separate
+// namespace from all of the variables and functions. So you could declare a
+// type called "list" and a variable called "list" in the same scope. But you
+// probably shouldn't.
 
 class Context {
-    constructor({ parent = null, currentFunction = null, inLoop = false } = {}) {
-        Object.assign(this, { parent, currentFunction, inLoop, declarations: Object.create(null) });
-    }
+  constructor({ parent = null, currentFunction = null, inLoop = false } = {}) {
+    Object.assign(this, {
+      parent,
+      currentFunction,
+      inLoop,
+      typeMap: Object.create(null),
+      valueMap: Object.create(null),
+    });
+  }
 
-    createChildContextForFunctionBody(currentFunction) {
-        // When entering a new function, we're not in a loop anymore
-        return new Context({ parent: this, currentFunction, inLoop: false });
-    }
+  createChildContextForFunctionBody(currentFunction) {
+    // When entering a new function, we're not in a loop anymore
+    return new Context({ parent: this, currentFunction, inLoop: false });
+  }
 
-    createChildContextForLoop() {
-        // When entering a loop body, just set the inLoop field, retain others
-        return new Context({ parent: this, currentFunction: this.currentFunction, inLoop: true });
-    }
+  createChildContextForLoop() {
+    // When entering a loop body, just set the inLoop field, retain others
+    return new Context({ parent: this, currentFunction: this.currentFunction, inLoop: true });
+  }
 
-    createChildContextForBlock() {
-        // For a simple block (i.e., in an if-statement), we have to retain both
-        // the function and loop settings.
-        return new Context({
-            parent: this,
-            currentFunction: this.currentFunction,
-            inLoop: this.inLoop,
-        });
-    }
+  createChildContextForBlock() {
+    // For a block, we have to retain both the function and loop settings.
+    return new Context({
+      parent: this,
+      currentFunction: this.currentFunction,
+      inLoop: this.inLoop,
+    });
+  }
 
-    // Call this to add a new entity (which could be a variable, a function,
-    // or a parameter) to this context. It will check to see if the entity's
-    // identifier has already been declared in this context. It does not need
-    // to check enclosing contexts because in this language, shadowing is always
-    // allowed. Note that if we allowed overloading, this method would have to
-    // be a bit more sophisticated.
-    add(entity) {
-        if (entity.id in this.declarations) {
-            throw new Error(`Identitier ${entity.id} already declared in this scope`);
-        }
-        this.declarations[entity.id] = entity;
+  // Adds a variable or function to this context.
+  add(entity) {
+    if (entity.id in this.valueMap) {
+      throw new Error(`${entity.id} already declared in this scope`);
     }
+    this.valueMap[entity.id] = entity;
+  }
 
-    // Returns the entity bound to the given identifier, starting from this
-    // context and searching "outward" through enclosing contexts if necessary.
-    lookup(id) {
-        if (id in this.declarations) {
-            return this.declarations[id];
-        } else if (this.parent === null) {
-            throw new Error(`Identifier ${id} has not been declared`);
-        } else {
-            return this.parent.lookup(id);
-        }
+  addType(typeDec) {
+    if (typeDec.id in this.typeMap) {
+      throw new Error(`Type ${typeDec.id} already declared in this scope`);
     }
+    this.typeMap[typeDec.id] = typeDec.type;
+  }
 
-    assertInFunction(message) {
-        if (!this.currentFunction) {
-            throw new Error(message);
-        }
+  // Returns the type entity bound to the given identifier, starting from this
+  // context and searching "outward" through enclosing contexts if necessary.
+  lookupType(id) {
+    for (let context = this; context !== null; context = context.parent) {
+      if (id in context.typeMap) {
+        return context.typeMap[id];
+      }
     }
+    throw new Error(`Type ${id} has not been declared`);
+  }
 
-    assertIsFunction(entity) { // eslint-disable-line class-methods-use-this
-        if (entity.constructor !== FunctionObject) {
-            throw new Error(`${entity.id} is not a function`);
-        }
+  // Returns the variable or function entity bound to the given identifier,
+  // starting from this context and searching "outward" through enclosing
+  // contexts if necessary.
+  lookupValue(id) {
+    for (let context = this; context !== null; context = context.parent) {
+      if (id in context.valueMap) {
+        return context.valueMap[id];
+      }
     }
+    throw new Error(`${id} has not been declared`);
+  }
 }
 
 Context.INITIAL = new Context();
-new FunctionDeclaration('print', [new Parameter('_', null)], null).analyze(Context.INITIAL);
-new FunctionDeclaration('sqrt', [new Parameter('_', null)], null).analyze(Context.INITIAL);
+standardFunctions.forEach((f) => { Context.INITIAL.valueMap[f.id] = f; });
+Context.INITIAL.typeMap.int = IntType;
+Context.INITIAL.typeMap.string = StringType;
+Context.INITIAL.typeMap.nil = NilType;
 
 module.exports = Context;
