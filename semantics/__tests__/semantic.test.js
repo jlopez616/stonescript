48e3ddb664d87a4f72a7a7ac2d82880c2bde78f8
aboyac <aboyac@lion.lmu.edu>
2019-04-26 22:33:11 -0700

    Removed tiger's version of semantic tests

--- semantics/__tests__/semantic.test.js
+++ semantics/__tests__/semantic.test.js
@@ -78,28 +78,6 @@ NOT PART!
 
 `;
 
-// const program = String.raw`
-// let
-//   type Circle = {
-//     x: int,
-//     y: int,
-//     color: string
-//   }
-//   type list = array of string
-//   var c: Circle := Circle {y = 2, x = 5<3&2<>1, color = "blue"}
-//   var dogs: list := list [3] of "woof"
-// in
-//   dogs[1] := "Sparky";
-//   for i := 1 to 10 do
-//     print(concat(chr(2), "xyz"))
-//     /*
-//      *
-//      * NEEDS A ZILLION MORE THINGS
-//      *
-//      */
-// end
-// `;
-
 describe('The semantic analyzer', () => {
   test('accepts the mega program with all syntactic forms', (done) => {
     const astRoot = parse(program);
