96739d93daa30fafd4093ec3b3cda47c0b7fd0c7
John Lopez <johnllopez616@gmail.com>
2019-05-07 23:59:27 -0700

    The great copy + paste

--- semantics/__tests__/semantic.test.js
+++ semantics/__tests__/semantic.test.js
@@ -11,19 +11,19 @@ const Context = require('../context');
 // TODO!!!!!
 
 const program = String.raw`
-🦕 
+🦕
       Author: John, Anthony, Kevin, Alex
       Password: **********
 
-      Introduction: Here are tests check to make this 
+      Introduction: Here are tests check to make this
       primitive language correctly accept this hideous
-      program that happily passes the semantic 
+      program that happily passes the semantic
       constraints set by our language.
       PS: This is an elaborate copy paste of the first
       multiline comment of this file :D
 
-      TODO: "Tablets" and "fields" are not currently 
-      implemented for the sake of this homework. 
+      TODO: "Tablets" and "fields" are not currently
+      implemented for the sake of this homework.
 🦕
 
 BEDROCK COUNTERS x IS 5!
@@ -35,7 +35,7 @@ YABBADABBADOO () PART
     ROCK COUNTERS count IS 10!
     🦖 Will loop ten times, printing "kratos" each time
     FOR (ROCK COUNTERS i IS 0! i NOT SMASH 10! i IS i SQUISH 1) PART
-        SPEAK(kratos)!        
+        SPEAK(kratos)!
     NOT PART!
     GIVE WHAT!
 NOT PART!
@@ -43,11 +43,11 @@ NOT PART!
 ROCK COUNTERS score IS 80!
 YABBADABBADOO () PART
     OOF (score SMASH OR IS 90) PART
-    SPEAK("Great job!")! 
+    SPEAK("Great job!")!
     NOT PART OOOF (score SMASH OR IS 80) PART
     SPEAK("Not bad.")!
     NOT PART OOOF (score SMASH OR IS 70) PART
-    SPEAK("You can do better.")! 
+    SPEAK("You can do better.")!
     NOT PART OOOF (score SMASH OR IS 60) PART
     SPEAK("What are you doing here?")!
     NOT PART OOFF PART
@@ -72,13 +72,13 @@ SPEAK(DALENGTH(waitASec))!
 
 ROCK COUNTERS randomNum IS 100!
 100 CUT 2!
-🦖 TODO: Line below doesn't work!!!!! 
+🦖 TODO: Line below doesn't work!!!!!
 🦖 ROCK COUNTERS bigop IS randomNum SQUISH 100 CUT 10 MANY 5 RIP 69 RIP 1 SQUISH 470 MANY 2 CUT 1000!
 🦖 output: 1
 
 
 ROCK YESNOS funbool IS OOGA!
-WHILE (funbool NOT IS funbool) PART 
+WHILE (funbool NOT IS funbool) PART
     🦖 Infinite loop? Nahhhhhh
     RAGEQUIT!
 NOT PART!
