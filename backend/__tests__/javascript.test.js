c8a2b701de1d98c651b09c84756764c826d59ee3
John Lopez <johnllopez616@gmail.com>
2019-05-08 00:01:01 -0700

    The great copy + paste

--- backend/__tests__/javascript.test.js
+++ backend/__tests__/javascript.test.js
@@ -16,17 +16,23 @@ const fixture = {
     String.raw`console.log("Hello, world");`
   ],
 
-/*  arithmetic: [String.raw`5 MANY -2 SQUISH 8`, String.raw`((5 * (-(2))) + 8)`],
+  arithmetic: [String.raw`5 MANY 2!`, String.raw `5 * 2;`],
 
   letAndAssign: [
-    String.raw`ROCK a IS "YA BOI"!`,
+    String.raw`ROCK WORDERS a IS "YA BOI"!`,
     String.raw`let a = "YA BOI";`
   ],
 
+  /* While loops to be added in Homework 5
   whileLoop: [
-    String.raw`WHILE 7 PART BREAK! NOT PART`,
+    String.raw`WHILE (OOGA) PART RAGEQUIT! NOT PART!`,
     /while \(7\) \{\s*break\s*\}/
-  ],
+  ]
+
+  list: [
+    String.raw`ROCK WORDERS CAVE lister IS CAVEIN "WORD" CAVEOUT!`,
+    String.raw`let lister = [ "WORD" ];`
+  ]
 
   forLoop: [
     String.raw`FOR ( ROCK i IS 0; I NOT SMASH N; I IS I SQUISH 1) PART BREAK NOT PART`,
@@ -37,23 +43,22 @@ const fixture = {
 
   ifThenElse: [String.raw`OOF 3 OOOF 5 OOFF 8`, "((3) ? (5) : (8))"],
 
-  list: [
-    String.raw`ROCK CAVES lister IS CAVEIN WORDERS "WORD" CAVEOUT`,
-    String.raw`let lister = [ "WORD" ]`
-  ],
+
 
   func: [
     String.raw`ROCK F IS YABBADABBADOO (COUNTERS N) GIVES COUNTERS PART N IS N SQUISH 1! GIVE N! NOT PART `,
     String.raw`let f = (n) => { n = n + 1; return n; }`
   ]
-  */
+ */
 };
 
 describe('The JavaScript generator', () => {
     Object.entries(fixture).forEach(([name, [source, expected]]) => {
         test(`produces the correct output for ${name}`, (done) => {
             const ast = parse(source);
+            console.log(ast);
             ast.analyze(Context.INITIAL);
+            console.log(ast);
             expect(generate(ast)).toMatch(expected);
             done();
         });
